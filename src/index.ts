e3478470f4fcdcb191c2ccad4d7a288b1282e9b1
云牧 <yunmucoder@163.com>
2022-06-29 16:49:18 +0800

    feat: 处理请求body的逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import { AxiosRequestConfig } from './types'
 import buildURL from './helpers/url'
 import xhr from './xhr'
+import { transformRequest } from './helpers/data'
 function axios(config: AxiosRequestConfig): void {
   processConfig(config)
+  console.log(config)
   xhr(config)
 }
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
 export default axios
