4566dc110f924f9ed1954df9acfad025ee9c34af
云牧 <yunmucoder@163.com>
2022-07-02 00:18:23 +0800

    feat: 扩展axios接口

--- src/axios.ts
+++ src/axios.ts
@@ -1,38 +1,16 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import buildURL from './helpers/url'
-import xhr from './xhr'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
+import { AxiosInstance } from './types'
+import Axios from './core/Axios'
+import { extend } from './helpers/util'
+
+function createInstance() {
+  const context = new Axios()
+  const instance = Axios.prototype.request.bind(context)
+  // instance 本身是一个函数，又拥有了 Axios 类的所有原型和实例属性
+  extend(instance, context)
+
+  return instance as AxiosInstance
 }
 
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformUrl(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
-}
-
-function transformUrl(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig) {
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformResponseData(res: AxiosResponse) {
-  res.data = transformResponse(res.data)
-  return res
-}
+const axios = createInstance()
 
 export default axios
