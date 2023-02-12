6c38d960b4fcb83eed0bdb9e17b2cd9b397cc7b0
yunumu <1416900346@qq.com>
2023-02-12 12:11:53 +0800

    feat: 自定义参数序列化

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -124,37 +124,40 @@ import NProgress from 'nprogress'
 //     console.log(e.message)
 //   })
 
-// axios.get('/more/get', {
-//   params: new URLSearchParams('a=b&c=d')
-// }).then(res => {
-//   console.log(res)
-// })
-//
-// axios.get('/more/get', {
-//   params: {
-//     a: 1,
-//     b: 2,
-//     c: ['a', 'b', 'c']
-//   }
-// }).then(res => {
-//   console.log(res)
-// })
-//
-// const instance = axios.create({
-//   paramsSerializer(params) {
-//     return qs.stringify(params, { arrayFormat: 'brackets' })
-//   }
-// })
-//
-// instance.get('/more/get', {
-//   params: {
-//     a: 1,
-//     b: 2,
-//     c: ['a', 'b', 'c']
-//   }
-// }).then(res => {
-//   console.log(res)
-// })
+axios
+  .get('/more/get', {
+    params: new URLSearchParams('a=b&c=d')
+  })
+  .then(res => {
+    console.log(res)
+  })
+
+axios
+  .get('/more/get', {
+    params: {
+      a: 1,
+      b: 2,
+      c: ['a', 'b', 'c']
+    }
+  })
+  .then(res => {
+    console.log(res)
+  })
+
+axios
+  .get('/more/get', {
+    params: {
+      a: 1,
+      b: 2,
+      c: ['a', 'b', 'c']
+    },
+    paramsSerializer(params) {
+      return qs.stringify(params, { arrayFormat: 'brackets' })
+    }
+  })
+  .then(res => {
+    console.log(res)
+  })
 
 // const instance = axios.create({
 //   baseURL: 'https://img.mukewang.com/'
